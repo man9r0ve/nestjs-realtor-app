e49dac6419fd1a5e95d048012276dca756d0432a
man9r0ve <mangrove.sh@gmail.com>
2023-07-09 00:12:11 +0900

    2023-07-09 query param 추가

--- src/home/dto/home.dto.ts
+++ src/home/dto/home.dto.ts
@@ -29,6 +29,8 @@ export class HomeResponseDto {
 
   price: number;
 
+  image: string;
+
   @Exclude()
   land_size: number;
 
--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,13 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HomeResponseDto } from './dto/home.dto';
+import { PropertyType } from '@prisma/client';
+
+interface GetHomesParam {
+  city?: string;
+  price?: {
+    gte?: number;
+    lte?: number;
+  };
+  propertyType?: PropertyType;
+}
 
 @Injectable()
 export class HomeService {
   constructor(private readonly prismaSerivce: PrismaService) {}
 
-  async getHomes(): Promise<HomeResponseDto[]> {
-    const homes = await this.prismaSerivce.home.findMany();
-    return homes.map((home) => new HomeResponseDto(home));
+  async getHomes(filters: GetHomesParam): Promise<HomeResponseDto[]> {
+    const homes = await this.prismaSerivce.home.findMany({
+      select: {
+        id: true,
+        address: true,
+        city: true,
+        price: true,
+        property_type: true,
+        number_of_bathrooms: true,
+        number_of_bedrooms: true,
+        images: {
+          select: {
+            url: true,
+          },
+          take: 1,
+        },
+      },
+      where: filters,
+    });
+
+    if (!homes.length) {
+      throw new NotFoundException();
+    }
+    return homes.map((home) => {
+      const fatchHome = { ...home, image: home.images[0].url };
+      delete fatchHome.images;
+      return new HomeResponseDto(fatchHome);
+    });
   }
 }
